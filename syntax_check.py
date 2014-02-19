1aff1a35b229ba57b952fc30ee6165c617a42c5e
Matt Martz <matt@sivel.net>
2014-02-19 13:18:21 -0600

    Catch more than ParserError

--- syntax_check.py
+++ syntax_check.py
@@ -5,7 +5,7 @@ for yml in sys.argv[1:]:
     try:
         with open(yml) as f:
             yaml.load(f)
-    except yaml.parser.ParserError as e:
+    except (yaml.parser.ParserError, yaml.scanner.ScannerError) as e:
         print '********\n%s\n********' % e
     else:
         print '%s: Syntax OK' % yml
