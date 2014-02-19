140f69890426bdb320dfda77f7763b4830ed70db
Matt Martz <matt@sivel.net>
2014-02-19 14:33:45 -0600

    Exit > 1 if errors are encountered

--- syntax_check.py
+++ syntax_check.py
@@ -1,11 +1,15 @@
 import sys
 import yaml
 
+exit = 0
 for yml in sys.argv[1:]:
     try:
         with open(yml) as f:
             yaml.load(f)
     except (yaml.parser.ParserError, yaml.scanner.ScannerError) as e:
         print '********\n%s\n********' % e
+        exit += 1
     else:
         print '%s: Syntax OK' % yml
+
+sys.exit(exit)
